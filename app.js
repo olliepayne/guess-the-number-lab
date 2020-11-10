106b1b91524359a72885b19016d516d887c4d38d
Oliver Payne <olliepayne@me.com>
2020-11-09 22:35:56 -0700

    tidy into objects

--- app.js
+++ app.js
@@ -3,65 +3,56 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
-  setSecretNum: function() {
+  init: function() {
     this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum;
+
+    player.guess = undefined;
+    player.previousGuesses = [];
+    player.setPlayerName();
+
+    game.renderGuessInput();
   },
-  numLowMessage: 'Your guess is too low!',
-  numHighMessage: 'Your guess is too high!',
+  renderGuessInput: function() {
+    let guessPrompt = prompt(`Guess a number between ${game.smallestNum} and ${game.biggestNum} ...Previous guesses: ${player.previousGuesses}`, 'Enter Here...');
+    if(!isNaN(guessPrompt)) {
+      player.guess = parseInt(guessPrompt);
+      player.previousGuesses.push(player.guess);
+      game.checkGuess();
+    } else {
+      alert('Guess needs to be a number');
+      game.renderGuessInput();
+    }
+  },
+  checkGuess: function() {
+    if(player.guess >= game.smallestNum && player.guess <= game.biggestNum) {
+      if(player.guess > game.secretNum) {
+        alert(`Your guess was too high! You guessed: ${player.guess}`);
+        game.renderGuessInput();
+      } else if(player.guess < game.secretNum) {
+        alert(`Your guess was too low! You guessed: ${player.guess}`);
+        game.renderGuessInput();
+      } else {
+        game.endGame();
+      }
+    } else {
+      alert(`Guess must be between ${game.smallestNum} and ${game.biggestNum}`);
+      game.renderGuessInput();
+    }
+  },
+  endGame: function() {
+    alert(`Congratulations ${player.name}! You guessed correctly with ${player.previousGuesses.length} tries! :O`);
+
+    game.init();
+  },
 };
 
 const player = {
   name: '',
   setPlayerName: function() {
-    this.name = prompt("What's your name ?", 'Enter here...');   
+    this.name = prompt("What's your name?", 'Enter Here...');
   },
+  guess: 0,
   previousGuesses: [],
-}
+};
 
-init();
-function init() {
-  game.setSecretNum();
-  console.log(game.secretNum);  // REMOVE
-
-  player.previousGuesses = [];
-  player.setPlayerName();
-
-  alert(`${game.title}`);
-  displayGuessInput();
-}
-
-function displayGuessInput() {
-  let guessPrompt = prompt(`Guess a number between ${game.smallestNum} and ${game.biggestNum} ...Previous guesses: ${player.previousGuesses}`, 'Enter here...');
-  if(!isNaN(guessPrompt)) {
-    const playerGuess = parseInt(guessPrompt);
-    checkGuess(playerGuess);
-  } else {
-    alert(`Input needs to be a number! ...Your Input: ${guessPrompt}`);
-    displayGuessInput();
-  }
-}
-
-function checkGuess(playerGuess) {
-  if(playerGuess >= game.smallestNum && playerGuess <= game.biggestNum) {
-    if(playerGuess < game.secretNum) {
-      alert(game.numLowMessage);
-      player.previousGuesses.push(playerGuess);
-      displayGuessInput();
-    } else if(playerGuess > game.secretNum) {
-      alert(game.numHighMessage);
-      player.previousGuesses.push(playerGuess);
-      displayGuessInput();
-    } else {
-      player.previousGuesses.push(playerGuess);
-      playerWins();
-    }
-  } else {
-    alert(`Number needs to be within ${game.smallestNum} and ${game.biggestNum}`);
-  }
-}
-
-function playerWins() {
-  alert(`Congratulations, ${player.name}! You guessed correctly with a total of ${player.previousGuesses.length} guesses! :O`);
-
-  init();
-}
+game.init();
